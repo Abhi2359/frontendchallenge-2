62efbf6b4e960f5d104742c56101b9ad631022ab
Abhi2359 <abhishekchoudhary1018@gmail.com>
2023-02-11 00:43:22 +0530

    commit

---
+++ .history/src/App_20230211002417.js
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar/Navbar";
+
+
+function App() {
+  return (
+    <div className="App">
+     <Navbar/>
+     <hr>
+    </div>
+  );
+}
+
+export default App;
---
+++ .history/src/components/Navbar/Navbar_20230210233226.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Navbar.css'
+import cart fro
+const Navbar = () => {
+  return (
+    <header>
+      <div className="navigation">
+
+      <nav className="navbar">
+        <ul>
+       <h1>sneakers</h1>
+       <li> <a href="#">Collections</a></li>
+      <li> <a href="#">Men</a></li>  
+       <li> <a href="#">Women</a></li>
+       <li> <a href="#">About</a></li>
+      <li> <a href="#">Contact</a></li> 
+        </ul>
+      </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
